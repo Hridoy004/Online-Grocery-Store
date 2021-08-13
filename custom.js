a12670f8977ad7049590b5659568829d2ca0df91
Sahojahan Miah Hridoy 191.092 <hridoyahmed2435@gmail.com>
2021-08-13 15:37:37 +0600

    javascript

--- custom.js
+++ custom.js
@@ -1,4 +1,3 @@
-
 let btnOpen = document.querySelector('label');
 let input = document.querySelector('input');
 
@@ -7,3 +6,23 @@ btnOpen.addEventListener('click', () =>{
 });
 
 
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-2');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 150){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+
+}
